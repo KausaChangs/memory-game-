34af4c3f4ccd6a42a69fa45602beb3e91f3e6056
KausaChangs <changayakausa@gmail.com>
2023-03-04 11:07:53 +0200

    populated cardArray

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-const cardArray = [];
+const cardArray = [
+  {
+    name: "fries",
+    img: "images/fries.png",
+  },
+  {
+    name: "cheeseburger",
+    img: "images/cheeseburger.png",
+  },
+  {
+    name: "hotdog",
+    img: "images/hotdog.png",
+  },
+  {
+    name: "blank",
+    img: "images/blank.png",
+  },
+  {
+    name: "ice-cream",
+    img: "images/ice-cream.png",
+  },
+  {
+    name: "milkshake",
+    img: "images/milkshake.png",
+  },
+  {
+    name: "pizza",
+    img: "images/pizza.png",
+  },
+  {
+    name: "white",
+    img: "images/white.png",
+  },
+];
