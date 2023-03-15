3fc1e2b39af840feb586aed52b6f097c7cbe2be1
KausaChangs <changayakausa@gmail.com>
2023-03-15 11:57:24 +0200

    created a results display variable

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random()); //this is a shortcut to shuffling an array randomly
 
 const gridDisplay = document.querySelector("#grid");
+const resultDisplay = document.querySelector("#result");
 
 let cardsChosen = [];
 
@@ -77,24 +78,28 @@ function checkMatch() {
   } else {
     cards[optionOneId].setAttribute("src", "images/blank.png");
     cards[optionTwoId].setAttribute("src", "images/blank.png");
-    alert("sorry try again!")
+    alert("sorry try again!");
   }
 
+  resultDisplay.textContent = cardsWon.length;
   cardsChosen = [];
   cardsChosenIds = [];
 
-  function flipCard() {
-    const cardId = this.getAttribute("data-id");
-    cardsChosen.push(cardArray[cardId].name);
-    cardsChosenIds.push(cardId);
+  if (cardsWon.length == cardArray.length / 2) {
+    resultDisplay.textContent = "Congratulations you found them all!";
+  }
+}
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
+  cardsChosen.push(cardArray[cardId].name);
+  cardsChosenIds.push(cardId);
 
-    console.log(cardsChosen);
-    console.log(cardsChosenIds);
+  console.log(cardsChosen);
+  console.log(cardsChosenIds);
 
-    this.setAttribute("src", cardArray[cardId].img);
+  this.setAttribute("src", cardArray[cardId].img);
 
-    if (cardsChosen.length === 2) {
-      setTimeout(checkMatch, 500);
-    }
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
   }
 }
